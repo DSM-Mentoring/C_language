c7cd83009f0ac466a38f548d97fa4e0dbcf0bfd1
하혜령 <gkgpfud1004@dsm.hs.kr>
2021-05-19 23:25:16 +0900

    [ADD] 1-10홀수출력

---
+++ 0519/1-10홀수출력(for).c
@@ -0,0 +1,14 @@
+//[문제]
+//1부터 10중에서 홀수를 줄별로 내림차순으로 출력하시오.
+//(for문 while문 둘 다 시도해보세요)
+
+//for문(실행X)
+/*#pragma warning (disable : 4996)
+#include <stdio.h>
+int main() {
+	for (int i = 10; i >= 1; i--) {
+		if(i % 2 == 1)
+			printf("%d\n", i);
+	}
+	return 0;
+}*/
