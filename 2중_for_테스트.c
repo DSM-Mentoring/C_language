fed280bd7f1722f943ecbdbf66f8f57e7b5ff9e6
하혜령 <gkgpfud1004@dsm.hs.kr>
2021-05-23 17:05:26 +0900

    [ADD] 2중_for문_test

---
+++ 2중_for_테스트.c
@@ -0,0 +1,17 @@
+﻿// 1
+// 1 2 3
+// 2
+// 1 2 3
+// 3
+// 1 2 3
+/*#pragma warning (disable : 4996)
+#include <stdio.h>
+int main() {
+	for (int i = 1; i <= 3; i++) {
+		printf("%d\n", i);
+		for (int j = 1; j <= 3; j++) {
+			printf("%d ", j);
+		}
+		printf("\n");
+	}
+}*/
