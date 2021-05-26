cefef2bbf8d8066e0cf001440b1b75f90ac8b7de
하혜령 <gkgpfud1004@dsm.hs.kr>
2021-05-26 23:05:13 +0900

    [ADD] 0526

---
+++ 이름나이출력.c
@@ -0,0 +1,14 @@
+//	이름, 나이 출력
+/*#pragma warning (disable : 4996)
+#include <stdio.h>
+int main() {
+	char name[30];
+	int age;
+
+	scanf("%s", &name);
+	scanf("%d", &age);
+	printf("%s", name);
+	printf("%d", age);
+	
+	return 0;
+}*/
